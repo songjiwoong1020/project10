f4aef10f120035aa93b9a2aa864c2c767c0b9d45
JiwoongSong <songjiwoong1020@gmail.com>
2025-02-10 13:42:01 +0900

    작업환경변경경

--- src/app/@articles/[keywords]/page.tsx
+++ src/app/@articles/[keywords]/page.tsx
@@ -1,8 +1,4 @@
-"use client";
-import { useParams } from "next/navigation";
-
-export default function keyword({ params }: any) {
-  const _params = useParams();
-  console.log("Current keywords:", _params.keywords);
-  return <div>{params}dsdsdsd</div>
+export default function keyword() {
+  console.log("Current keywords:");
+  return <div>dsdsdsd</div>
 }
